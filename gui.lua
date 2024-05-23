71540a400b65304f643b9522fca2b3c8d321cf4d
ElectronApps <barbwirek4@gmail.com>
2024-05-23 22:34:58 +0800

    Implement somewhat working use_state

--- gui.lua
+++ gui.lua
@@ -30,7 +30,7 @@ local global_events = {}
 
 ---@class DrawableElement
 ---@field type "drawable"
----@field element render_function
+---@field render render_function
 ---@field max_width integer
 ---@field max_height integer
 ---@field events table<string, function>?
@@ -63,7 +63,7 @@ end
 function gui.create_drawable_element(render, max_width, max_height, events, key)
     return {
         type = "drawable",
-        element = render,
+        render = render,
         max_width = max_width,
         max_height = max_height,
         events = events,
@@ -78,12 +78,16 @@ end
 ---@return { node_key: string, frame: { x: integer, y: integer, w: integer, h: integer } }
 local function recursive_render(node, max_width, max_height, coordinates_callback)
     local node_key = global_parent .. "_" .. global_id
+    
     if node.key ~= nil then
         node_key = node_key .. "_key_" .. node.key
     else
         global_id = global_id + 1
     end
-    node_key = node_key .. "_" .. string.sub(tostring(node.element), 11)
+
+    if node.type == "composable" then
+        node_key = node_key .. "_" .. string.sub(tostring(node.element), 11)
+    end
     
     if node.events ~= nil then
         global_events[node_key] = node.events
@@ -95,11 +99,18 @@ local function recursive_render(node, max_width, max_height, coordinates_callbac
     global_parent = node_key
     global_id = 0
 
-    global_context[node_key] = {
-        pass_frame = true,
-        local_frame = {},
-        children = {}
-    }
+    if global_context[node_key] == nil then
+        global_context[node_key] = {
+            rendered = true,
+            states = {},
+            pass_frame = true,
+            local_frame = {},
+            children = {}
+        }
+    else
+        global_context[node_key].rendered = true
+        global_context[node_key].children = {}
+    end
 
     if node.type == "composable" then
         local child_info = recursive_render(node.element(node.props), max_width, max_height, coordinates_callback)
@@ -114,7 +125,7 @@ local function recursive_render(node, max_width, max_height, coordinates_callbac
         local buffer = gpu.allocateBuffer(width, height)
         gpu.setActiveBuffer(buffer)
 
-        width, height = node.element(width, height, function(element, max_width, max_height, coordinates_callback)
+        width, height = node.render(width, height, function(element, max_width, max_height, coordinates_callback)
             local child_info = recursive_render(element, max_width, max_height, coordinates_callback)
             table.insert(global_context[node_key].children, child_info.node_key)
         end)
@@ -160,28 +171,70 @@ local function recursive_calc_frame(node_key, frame)
     end
 end
 
----@param start_node fun(): Element
-function gui.start(start_node)
+---@type fun(): Element
+local global_component
+
+local function internal_render()
     local status, err = pcall(function()
-        local w, h = gpu.getResolution()
+        local w, h = gpu.getViewport()
+        
+        gpu.fill(1, 1, w, h, " ")
 
         global_id = 0
-        global_parent = string.sub(tostring(start_node), 11)
-        global_context = {}
+        global_parent = string.sub(tostring(global_component), 11)
         global_events = {}
 
-        local child_info = recursive_render(start_node(), w, h, function(width, height)
+        for _, context in pairs(global_context) do
+            context.rendered = false
+        end
+
+        if global_context[string.sub(tostring(global_component), 11)] ~= nil then
+            global_context[string.sub(tostring(global_component), 11)].rendered = true
+        else
+            global_context[string.sub(tostring(global_component), 11)] = {
+                rendered = true,
+                states = {},
+                pass_frame = true,
+                local_frame = {
+                    x = 1,
+                    y = 1,
+                    w = w,
+                    h = h
+                },
+                global_frame = {
+                    x = 1,
+                    y = 1,
+                    w = w,
+                    h = h
+                },
+                children = {}
+            }
+        end
+
+        local child_info = recursive_render(global_component(), w, h, function(width, height)
             return 1, 1
         end)
 
         global_context[child_info.node_key].global_frame = child_info.frame
         recursive_calc_frame(child_info.node_key, child_info.frame)
+
+        global_context = util.filter(global_context, function(context)
+            return context.rendered
+        end)
     end)
 
     if not status then
         gpu.freeAllBuffers()
         print(err)
     end
+end
+
+---@param start_node fun(): Element
+function gui.start(start_node)
+    global_context = {}
+
+    global_component = start_node
+    internal_render()
 
     while true do
         local id, _, x, y = event.pullMultiple("touch", "interrupted")
@@ -194,12 +247,49 @@ function gui.start(start_node)
                         local frame = global_context[node_key].global_frame
                         if x >= frame.x and x < frame.x  + frame.w and y >= frame.y and y < frame.y + frame.h then
                             callback()
+                            goto outer_break -- break out of the outer loop
                         end
                     end
                 end
             end
+            ::outer_break::
         end
     end
+end
+
+---@generic T
+---@param initial_state T
+---@return T, fun(T)
+function gui.use_state(initial_state)
+    local parent = global_parent
+    local id = global_id
+
+    global_id = global_id + 1
+
+    local current_context = global_context[parent]
+    if current_context == nil then
+        current_context = {
+            rendered = true,
+            states = {},
+            pass_frame = true,
+            local_frame = {},
+            children = {}
+        }
+        global_context[parent] = current_context
+    end
+
+    local current_state = current_context.states[id]
+    if current_state == nil then
+        current_state = initial_state
+        current_context.states[id] = current_state
+    end
+
+    local set_state = function (new_value)
+        current_context.states[id] = new_value
+        internal_render()
+    end
+
+    return current_state, set_state
 end
 
 return gui
