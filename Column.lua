f58c2a4f0737dc95aeffa8278ff9fed54ef40315
ElectronApps <klimchikeg@gmail.com>
2024-05-23 20:12:09 +0800

    Implement basic event handling

--- Column.lua
+++ Column.lua
@@ -1,7 +1,9 @@
+local math = require("math")
 local gui = require("gregui.gui")
 
 ---@class ColumnProps
 ---@field children Element[]
+---@field fill_max_width boolean
 ---@field horizontal_alignment "left" | "center" | "right"
 
 ---@param props ColumnProps
@@ -9,17 +11,22 @@ return function(props)
     setmetatable(props, {
         __index = {
             children = {},
+            fill_max_width = true,
             horizontal_alignment = "left"
         }
     })
 
     return gui.create_drawable_element(function(width, height, render_callback)
-        local y = 1
+        local w = 0
         local h = 0
         for _, child in pairs(props.children) do
-            render_callback(child, width, height - y + 1, function(child_width, child_height)
-                h = child_height
-                if props.horizontal_alignment == "left" then
+            render_callback(child, width, height - h, function(child_width, child_height)
+                local y = h + 1
+                w = math.max(w, child_width)
+                h = h + child_height
+                if not props.fill_max_width then
+                    return 1, y
+                elseif props.horizontal_alignment == "left" then
                     return 1, y
                 elseif props.horizontal_alignment == "center" then
                     return (width - child_width) // 2 + 1, y
@@ -27,8 +34,12 @@ return function(props)
                     return width - child_width + 1, y
                 end
             end)
-            y = y + h
         end
-        return width, y - 1
+        
+        if props.fill_max_width then
+            return width, h
+        else
+            return w, h
+        end
     end, -1, -1)
 end
--- util.lua
+++ util.lua
@@ -12,4 +12,30 @@ function util.contains(table, key)
     return false
 end
 
+---@generic T, E, U
+---@param table table<T, E>
+---@param f fun(E): U
+---@return table<T, U>
+function util.map(table, f)
+  local result = {}
+  for k, v in pairs(table) do
+    result[k] = f(v)
+  end
+  return result
+end
+
+---@generic T, E
+---@param table table<T, E>
+---@param f fun(E): boolean
+---@return table<T, E>
+function util.filter(table, f)
+  local result = {}
+  for k, v in pairs(table) do
+    if f(v) then
+      result[k] = v
+    end
+  end
+  return result
+end
+
 return util
